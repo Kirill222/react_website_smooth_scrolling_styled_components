c45dd52033d35c07262102b1a77d0c3d7a2ef8c1
Zzz <zkmd2012@inbox.ru>
2022-02-08 18:15:34 +0100

    basename={process.env.PUBLIC_URL} added to BrowserRouter to solve the problem with a blank page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css'
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages'
 import SigninPage from './pages/signin'
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/signin' component={SigninPage}/>
+        <Route exact path='/' component={Home} />
+        <Route path='/signin' component={SigninPage} />
       </Switch>
-      
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
