5531e02e2485f111536a6bb672079262e33bd3a8
Zzz <zkmd2012@inbox.ru>
2022-02-08 17:50:22 +0100

    prepared to upload to gh-pages

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,105 +1,110 @@
 import React from 'react'
-import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa'
-import {animateScroll as scroll} from 'react-scroll'
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa'
+import { animateScroll as scroll } from 'react-scroll'
 
 import {
-    FooterContainer,
-    FooterWrap,
-    FooterLinksContainer,
-    FooterLinksWrapper,
-    FooterLinkItems,
-    FooterLinkTitle,
-    FooterLink,
-    SocialMedia,
-    SocialMediaWrap,
-    SocialLogo,
-    WebsiteRights,
-    SocialIcons,
-    SocialIconLink,
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
 } from './FooterElements'
 
 const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop()
+  }
 
-    const toggleHome = () => {
-        scroll.scrollToTop()
-    }
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
 
-    return (
-        <FooterContainer>
-            <FooterWrap>
-                <FooterLinksContainer>
-                    <FooterLinksWrapper>    
-                        <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to='/'>How it works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinkItems>
 
-                            <FooterLink to="/">How it works</FooterLink>
-                            <FooterLink to="/">Testimonials</FooterLink>
-                            <FooterLink to="/">Careers</FooterLink>
-                            <FooterLink to="/">Investors</FooterLink>
-                            <FooterLink to="/">Terms of Service</FooterLink>                            
-                        </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
 
-                        <FooterLinkItems>
-                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to='/'>Contact</FooterLink>
+              <FooterLink to='/'>Support</FooterLink>
+              <FooterLink to='/'>Destinations</FooterLink>
+              <FooterLink to='/'>Sponsorships</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
 
-                            <FooterLink to="/">Contact</FooterLink>
-                            <FooterLink to="/">Support</FooterLink>
-                            <FooterLink to="/">Destinations</FooterLink>
-                            <FooterLink to="/">Sponsorships</FooterLink>
-                        </FooterLinkItems>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
 
-                        
-                    </FooterLinksWrapper>
+              <FooterLink to='/'>Submit video</FooterLink>
+              <FooterLink to='/'>Ambassadors</FooterLink>
+              <FooterLink to='/'>Agency</FooterLink>
+              <FooterLink to='/'>Influencers</FooterLink>
+            </FooterLinkItems>
 
-                    <FooterLinksWrapper>    
-                        <FooterLinkItems>
-                            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
 
-                            <FooterLink to="/">Submit video</FooterLink>
-                            <FooterLink to="/">Ambassadors</FooterLink>
-                            <FooterLink to="/">Agency</FooterLink>
-                            <FooterLink to="/">Influencers</FooterLink>
-                        </FooterLinkItems>
+              <FooterLink to='/'>Instagram</FooterLink>
+              <FooterLink to='/'>YouTube</FooterLink>
+              <FooterLink to='/'>Facebook</FooterLink>
+              <FooterLink to='/'>Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
 
-                        <FooterLinkItems>
-                            <FooterLinkTitle>Social Media</FooterLinkTitle>
-
-                            <FooterLink to="/">Instagram</FooterLink>
-                            <FooterLink to="/">YouTube</FooterLink>
-                            <FooterLink to="/">Facebook</FooterLink>
-                            <FooterLink to="/">Twitter</FooterLink>
-                        </FooterLinkItems>
-
-
-                    </FooterLinksWrapper>
-                </FooterLinksContainer>
-
-                <SocialMedia>
-                    <SocialMediaWrap>
-                        <SocialLogo to="/" onClick={toggleHome}>dolla</SocialLogo>
-                        <WebsiteRights>dolla &copy; {new Date().getFullYear()}. All rights reserved.</WebsiteRights>
-                        <SocialIcons>
-                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
-                                <FaFacebook />
-                            </SocialIconLink>
-                            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
-                                <FaInstagram />
-                            </SocialIconLink>
-                            <SocialIconLink href="/" target="_blank" aria-label="Youtube">
-                                <FaYoutube />
-                            </SocialIconLink>
-                            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
-                                <FaTwitter />
-                            </SocialIconLink>
-                            <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
-                                <FaLinkedin />
-                            </SocialIconLink>
-                        </SocialIcons>
-                    </SocialMediaWrap>
-                </SocialMedia>
-            </FooterWrap>
-        </FooterContainer>
-    )
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/' onClick={toggleHome}>
+              eBank
+            </SocialLogo>
+            <WebsiteRights>
+              eBank &copy; {new Date().getFullYear()}. All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  )
 }
 
 export default Footer
